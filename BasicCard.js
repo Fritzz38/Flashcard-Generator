29a036ab6376cb6bb9a98c6f584d3c718668384a
Tarmin Sidharta <tsidharta@gmail.com>
2017-07-01 11:01:45 -0700

    submitted

---
+++ BasicCard.js
@@ -0,0 +1,22 @@
+// This file should define a Node module that exports a constructor for creating basic flashcards, e.g.:
+
+// The constructor should accept two arguments: front and back.
+// The constructed object should have a front property that contains the text on the front of the card.
+// The constructed object should have a back property that contains the text on the back of the card.
+
+function BasicCard (front, back) {
+this.front = front;
+this.back = back
+}
+
+var firstPresident = new BasicCard("Who was the first president of the United States?", "George Washington");
+
+
+// "Who was the first president of the United States?"
+console.log(firstPresident.front); 
+
+// "George Washington"
+console.log(firstPresident.back); 
+
+
+module.exports = BasicCard;
